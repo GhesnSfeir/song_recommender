323e271e824212c766148c6bf95ebb1400b7cee9
Ghesn Sfeir <gslivedt@gmail.com>
2018-01-03 20:12:41 +0100

    Add additional songs and labels

--- songs.pl
+++ songs.pl
@@ -1,13 +1,24 @@
 label("rock", 0).
 label("ska", 0).
 label("house", 0).
+label("salsa", 0).
+label("ballad", 0).
 
 label("Spanish", 0).
 label("English", 0).
 label("none", 0).
 
 label("ACDC", 0).
+label("John Lennon", 0).
+label("Oscar de Leon", 0).
+label("The Beatles", 0).
 label("La Pegatina", 0).
 
 song("Back in black", "rock", "English", "ACDC").
+song("Thunderstruck", "rock", "English", "ACDC").
+song("TNT", "rock", "English", "ACDC").
+song("Imagine", "ballad", "English", "John Lennon").
+song("Lloraras", "salsa", "Spanish", "Oscar de Leon").
+song("Let it be", "ballad", "English", "The Beatles").
+song("Michelle", "ballad", "English", "The Beatles").
 song("Tomasin", "ska", "Spanish", "La Pegatina").
